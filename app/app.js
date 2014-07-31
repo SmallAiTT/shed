6d1dc3371daea0dcff883b7871c6c8c0ef583666
SmallAiTT <536762164@qq.com>
2014-07-31 23:12:21 +0800

    提交改动。

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var logger = require('morgan');
 var app = module.exports = express();
 var bodyParser = require('body-parser');
+var path = require("path");
 
 // app middleware
 app.engine('.html', require('ejs').__express);
@@ -9,8 +10,11 @@ app.engine('.html', require('ejs').__express);
 //配置
 var ttCfg = require("tt-cfg");
 var expressCfg = ttCfg.expressCfg;
-app.use(express.static(expressCfg.staticDir));
-ttCfg.embed(app);
+app.use(express.static( path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "html");
+//ttCfg.embed(app);
+
 require("tt-msg").embed(app);
 require("tt-err").embed(app, function(){
     app.use(bodyParser());
